806a4cf0e788ef918fd74318af8e0419613420e0
tasmiajabad4142-prog <jabad2626@gmail.com>
2026-09-25 11:57:47 +0600

    second commit

--- fit-log/src/app/components/Navbar.tsx
+++ fit-log/src/app/components/Navbar.tsx
@@ -9,20 +9,20 @@ const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
     return (
 
-        <header className='border-b border-[#272b33] bg-[#0d0f12]'>
-            <nav className='relative flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8'>
+        <header className=' border-b border-(--border) bg-(--primary)'>
+            <nav className='container mx-auto relative flex max-w-7xl items-center justify-between px-2 py-4 sm:px-6 lg:px-8'>
                 <Link href="/" className='flex items-center gap-2'>
                     <Image src="/logo.png" alt='Fit Log' width={20}
                         height={20}
-                        className='h-5 w-5'
+                        className='h-6 w-6'
                     />
-                    <span className='text-s text-white font-medium'>FITLOG</span>
+                    <span className='text-xl text-(--primary-dark) font-medium'>FITLOG</span>
                 </Link>
 
                 <div className="absolute left-1/2 -translate-x-1/2 items-center hidden gap-2 lg:flex">
 
-                    <Link href="/" className='rounded-full bg-[#ccff00] px-4 py-1 text-xs font-medium text-black'>Workouts</Link>
-                    <Link href="/my-plan" className='text-xs font-medium text-[#8b909a] transition hover:text-white'>My plan</Link>
+                    <Link href="/" className='rounded-full bg-(--primary-soft) px-4 py-1 text-[15px] font-medium text-(--primary-dark) hover:text-white'>Workouts</Link>
+                    <Link href="/my-plan" className='text-[15px] font-medium text-(--purple) transition hover:text-white'>My plan</Link>
 
                 </div>
 
@@ -31,11 +31,11 @@ const Navbar = () => {
 
                 </div>
                 <div className="hidden items-center lg:flex gap-2">
-                    <Link href="/my-plan" className='flex items-center gap-1 text-xs text-[#8b909a]'>Plan <span className='flex h-4 min-w-3 items-center justify-center rounded-full bg-[#ccff00] px-1 text-[10px] font-semibold text-black'>0</span></Link>
-                    <Link href="/my-plan" className='flex items-center gap-1.5 text-xs text-[#8b909a]'>Saved <span className='flex h-4 min-w-3 items-center justify-center rounded-full bg-[#ccff00] px-1 text-[10px] font-semibold text-black'>0</span></Link>
+                    <Link href="/my-plan" className='flex items-center gap-1 text-[15px] text-(--purple)'>Plan <span className='flex h-5 min-w-5 items-center justify-center rounded-full bg-(--primary-soft) px-1.5 text-[12px] font-semibold text-(--primary-dark)'>0</span></Link>
+                    <Link href="/my-plan" className='flex items-center gap-1.5 text-[15px] text-(--purple)'>Saved <span className='flex h-5 min-w-5 items-center justify-center rounded-full border border-(--primary-dark) px-1.5 text-[12px] font-semibold text-(--purple)'>0</span></Link>
                 </div>
                 <button type="button"
-                    onClick={() => setMenuOpen(!menuOpen)} className='flex h-9 w-9 items-center justify-center rounded-md border border-[#272b33] text-white lg:hidden'
+                    onClick={() => setMenuOpen(!menuOpen)} className='flex h-9 w-9 items-center justify-center rounded-md border border-[#272b33] text-white lg:hidden cursor-pointer'
                     aria-label='Toggle menu'
                     aria-expanded={menuOpen}>{menuOpen ? "✕" : "☰"}</button>
             </nav>
@@ -43,18 +43,19 @@ const Navbar = () => {
                 <div className='border-t border-[#272b33] px-4 py-4 lg:hidden'>
                     <div className="mx-auto flex max-w-7xl flex-col gap-2">
                         <Link href="/"
-                            onClick={() => setMenuOpen(false)} className='bg-[#ccff00] px-4 py-3 text-xs font-medium text-black'>Workouts</Link>
+                            onClick={() => setMenuOpen(false)} className='rounded-full bg-(--primary-soft) px-4 py-1 text-[15px] font-medium text-(--primary-dark) hover:text-white'>Workouts</Link>
+
                         <Link href="/my-plan"
                             onClick={() => setMenuOpen(false)}
-                            className="rounded-lg px-4 py-3 text-xs font-medium text-[#8b909a] hover:bg-[#15181e] hover:text-white">My Plan
+                            className="rounded-lg px-4 py-3 text-[15px] font-medium text-(--purple)  hover:text-white">My Plan
                         </Link>
                         <Link
                             href="/my-plan"
                             onClick={() => setMenuOpen(false)}
-                            className="flex items-center gap-2 text-xs text-[#8b909a]"
+                            className="flex items-center gap-2 text-[15px] text-(--purple)"
                         >
                             Plan
-                            <span className="flex h-5 min-w-5 items-center justify-center rounded-full bg-[#ccff00] px-1.5 text-[10px] font-semibold text-black">
+                            <span className="flex h-5 min-w-5 items-center justify-center rounded-full bg-(--primary) px-1.5 text-[12px] font-semibold text-(--primary-dark)">
                                 0
                             </span>
                         </Link>
@@ -62,10 +63,10 @@ const Navbar = () => {
                         <Link
                             href="/my-plan"
                             onClick={() => setMenuOpen(false)}
-                            className="flex items-center gap-2 text-xs text-[#8b909a]"
+                            className="flex items-center gap-1 text-[15px] text-(--purple)"
                         >
                             Saved
-                            <span className="flex h-5 min-w-5 items-center justify-center rounded-full border border-[#3a3f48] px-1.5 text-[10px]">
+                            <span className="flex h-5 min-w-5 items-center justify-center rounded-full border border-(--primary-dark) px-1.5 text-[12px] font-semibold text-(--purple)">
                                 0
                             </span>
                         </Link>
--- fit-log/src/app/components/page.tsx
+++ fit-log/src/app/components/page.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Navbar from './Navbar';
 
-const page = () => {
+
+export default function Home() {
     return (
-        <div>
-            <Navbar />
-        </div>
-    );
-};
+        <main className="min-h-screen bg-[#ffffff]">
 
-export default page;
+            <section className="mx-auto max-w-7xl px-4 py-20 sm:px-6 lg:px-8">
+                <h1 className="text-4xl font-bold text-white">
+                    FITLOG
+                </h1>
+
+                <p className="mt-4 text-[#8b909a]">
+                    Workout library and fitness planner.
+                </p>
+            </section>
+        </main>
+    );
+}
